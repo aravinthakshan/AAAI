298e0e1191f37216b535468aeea53a49979df995
aravinthakshan <aaravinth2005@gmail.com>
2025-08-20 20:43:49 +0530

    dataset directory structure updated

--- config.py
+++ config.py
@@ -19,24 +19,46 @@ class Config:
         self.min_lr = 1e-7
 
 
+# class DataPath:
+#     def __init__(self, dataset_dir):
+#         self.dataset_dir = dataset_dir
+
+#         ''' Train Dataset: CAMO-Train + COD10K-Train '''
+#         self.train_imgs = os.path.join(self.dataset_dir, 'TrainDataset', 'Imgs')
+#         self.train_masks = os.path.join(self.dataset_dir, 'TrainDataset', 'GT')
+
+#         ''' Test Dataset '''
+#         # CHAMELEON
+#         self.test_CHAMELEON_imgs = os.path.join(self.dataset_dir, 'TestDataset', 'CHAMELEON', 'Imgs')
+#         self.test_CHAMELEON_masks = os.path.join(self.dataset_dir, 'TestDataset', 'CHAMELEON', 'GT')
+#         # CAMO-Test
+#         self.test_CAMO_imgs = os.path.join(self.dataset_dir, 'TestDataset', 'CAMO', 'Imgs')
+#         self.test_CAMO_masks = os.path.join(self.dataset_dir, 'TestDataset', 'CAMO', 'GT')
+#         # COD10K-Test
+#         self.test_COD10K_imgs = os.path.join(self.dataset_dir, 'TestDataset', 'COD10K', 'Imgs')
+#         self.test_COD10K_masks = os.path.join(self.dataset_dir, 'TestDataset', 'COD10K', 'GT')
+#         # NC4K
+#         self.test_NC4K_imgs = os.path.join(self.dataset_dir, 'TestDataset', 'NC4K', 'Imgs')
+#         self.test_NC4K_masks = os.path.join(self.dataset_dir, 'TestDataset', 'NC4K', 'GT')
+
 class DataPath:
     def __init__(self, dataset_dir):
         self.dataset_dir = dataset_dir
 
         ''' Train Dataset: CAMO-Train + COD10K-Train '''
-        self.train_imgs = os.path.join(self.dataset_dir, 'TrainDataset', 'Imgs')
-        self.train_masks = os.path.join(self.dataset_dir, 'TrainDataset', 'GT')
+        self.train_imgs = os.path.join(self.dataset_dir, 'COD-TrainDataset', 'COD-TrainDataset', 'Imgs')
+        self.train_masks = os.path.join(self.dataset_dir, 'COD-TrainDataset', 'COD-TrainDataset', 'GT')
 
         ''' Test Dataset '''
         # CHAMELEON
-        self.test_CHAMELEON_imgs = os.path.join(self.dataset_dir, 'TestDataset', 'CHAMELEON', 'Imgs')
-        self.test_CHAMELEON_masks = os.path.join(self.dataset_dir, 'TestDataset', 'CHAMELEON', 'GT')
+        self.test_CHAMELEON_imgs = os.path.join(self.dataset_dir, 'COD-TestDataset', 'COD-TestDataset', 'CHAMELEON', 'Imgs')
+        self.test_CHAMELEON_masks = os.path.join(self.dataset_dir, 'COD-TestDataset', 'COD-TestDataset', 'CHAMELEON', 'GT')
         # CAMO-Test
-        self.test_CAMO_imgs = os.path.join(self.dataset_dir, 'TestDataset', 'CAMO', 'Imgs')
-        self.test_CAMO_masks = os.path.join(self.dataset_dir, 'TestDataset', 'CAMO', 'GT')
+        self.test_CAMO_imgs = os.path.join(self.dataset_dir, 'COD-TestDataset', 'COD-TestDataset', 'CAMO', 'Imgs')
+        self.test_CAMO_masks = os.path.join(self.dataset_dir, 'COD-TestDataset', 'COD-TestDataset', 'CAMO', 'GT')
         # COD10K-Test
-        self.test_COD10K_imgs = os.path.join(self.dataset_dir, 'TestDataset', 'COD10K', 'Imgs')
-        self.test_COD10K_masks = os.path.join(self.dataset_dir, 'TestDataset', 'COD10K', 'GT')
+        self.test_COD10K_imgs = os.path.join(self.dataset_dir, 'COD-TestDataset', 'COD-TestDataset', 'COD10K', 'Imgs')
+        self.test_COD10K_masks = os.path.join(self.dataset_dir, 'COD-TestDataset', 'COD-TestDataset', 'COD10K', 'GT')
         # NC4K
-        self.test_NC4K_imgs = os.path.join(self.dataset_dir, 'TestDataset', 'NC4K', 'Imgs')
-        self.test_NC4K_masks = os.path.join(self.dataset_dir, 'TestDataset', 'NC4K', 'GT')
+        self.test_NC4K_imgs = os.path.join(self.dataset_dir, 'COD-TestDataset', 'COD-TestDataset', 'NC4K', 'Imgs')
+        self.test_NC4K_masks = os.path.join(self.dataset_dir, 'COD-TestDataset', 'COD-TestDataset', 'NC4K', 'GT')
