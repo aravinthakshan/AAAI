20ff3909a5f42f325ddee889015bfca22a9d3e4c
aravinthakshan <aaravinth2005@gmail.com>
2025-08-21 14:15:29 +0530

    debug mode return 100 and epochs 2

--- utils/dataloader_freq.py
+++ utils/dataloader_freq.py
@@ -152,6 +152,7 @@ class TrainDataset(Dataset):
         return high, low
 
     def __len__(self):
+        return 100
         return self.size
 
 
@@ -192,6 +193,7 @@ class TestDataset(Dataset):
         self.freq_norm = transforms.Normalize(mean=freq_stats['mean'], std=freq_stats['std'])
 
     def __len__(self):
+        return 100
         return len(self.images)
 
     def __getitem__(self, index):
